0c7e8f65d330f795ea89b9ab7f30fc5647a74996
nenu <nehuennavarro@gmail.com>
2022-11-02 14:58:58 -0300

    test3

--- react-backend/client/src/usuarios/tablero.js
+++ react-backend/client/src/usuarios/tablero.js
@@ -1,253 +1,191 @@
-import React, { useCallback, useMemo, useState } from 'react';
-import MaterialReactTable from 'material-react-table';
-import {
-  Box,
-  Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
-  IconButton,
-  MenuItem,
-  Stack,
-  TextField,
-  Tooltip,
-} from '@mui/material';
-import { Delete, Edit } from '@mui/icons-material';
-import { data, states } from './makeData.ts';
 
-const Tablero = () => {
-  const [createModalOpen, setCreateModalOpen] = useState(false);
-  const [tableData, setTableData] = useState(() => data);
-  const [validationErrors, setValidationErrors] = useState({});
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";                                //icons
+ 
+import '../styles/index.css';
+import React, { Component } from 'react';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { InputText } from 'primereact/inputtext';
+import { InputNumber } from 'primereact/inputnumber';
+import { Dropdown } from 'primereact/dropdown';
+import { Button } from 'primereact/button';
+import { Toast } from 'primereact/toast';
+import { ProductService } from './ProductService';
+import '/opt/tableroAPP2/react-backend/client/src/tablero/DataTableDemo.css';
 
-  const handleCreateNewRow = (values) => {
-    tableData.push(values);
-    setTableData([...tableData]);
-  };
+export default class Tabla extends Component {
 
-  const handleSaveRowEdits = async ({ exitEditingMode, row, values }) => {
-    if (!Object.keys(validationErrors).length) {
-      tableData[row.index] = values;
-      //send/receive api updates here, then refetch or update local table data for re-render
-      setTableData([...tableData]);
-      exitEditingMode(); //required to exit editing mode and close modal
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            products1: null,
+            products2: null,
+            products3: null,
+            products4: null,
+            editingRows: {}
+        };
+
+        this.columns = [
+            { field: 'idtableros', header: 'idtableros' },
+            { field: 'nombre', header: 'nombre' },
+            { field: 'url', header: 'url' },
+            { field: 'accessToken', header: 'accessToken' },
+            { field: 'id', header: 'Id' }
+        ];
+
+        this.statuses = [
+            { label: 'In Stock', value: 'INSTOCK' },
+            { label: 'Low Stock', value: 'LOWSTOCK' },
+            { label: 'Out of Stock', value: 'OUTOFSTOCK' }
+        ];
+
+        this.productService = new ProductService();
+
+        this.statusBodyTemplate = this.statusBodyTemplate.bind(this);
+        this.priceBodyTemplate = this.priceBodyTemplate.bind(this);
+        this.onCellEditComplete = this.onCellEditComplete.bind(this);
+        this.onRowEditComplete1 = this.onRowEditComplete1.bind(this);
+        this.onRowEditComplete2 = this.onRowEditComplete2.bind(this);
+        this.onRowEditChange = this.onRowEditChange.bind(this);
     }
-  };
 
-  const handleDeleteRow = useCallback(
-    (row) => {
-      if (
-        !window.confirm(`Are you sure you want to delete ${row.getValue('firstName')}`)
-      ) {
-        return;
-      }
-      //send api delete request here, then refetch or update local table data for re-render
-      tableData.splice(row.index, 1);
-      setTableData([...tableData]);
-    },
-    [tableData],
-  );
+    componentDidMount() {
+        this.fetchProductData('products4');
+    }
 
-  const getCommonEditTextFieldProps = useCallback(
-    (cell) => {
-      return {
-        error: !!validationErrors[cell.id],
-        helperText: validationErrors[cell.id],
-        onBlur: (event) => {
-          const isValid =
-            cell.column.id === 'email'
-              ? validateEmail(event.target.value)
-              : cell.column.id === 'age'
-              ? validateAge(+event.target.value)
-              : validateRequired(event.target.value);
-          if (!isValid) {
-            //set validation error for cell if invalid
-            setValidationErrors({
-              ...validationErrors,
-              [cell.id]: `${cell.column.columnDef.header} is required`,
-            });
-          } else {
-            //remove validation error for cell if valid
-            delete validationErrors[cell.id];
-            setValidationErrors({
-              ...validationErrors,
-            });
-          }
-        },
-      };
-    },
-    [validationErrors],
-  );
+    fetchProductData(productStateKey) {
+        this.productService.getProductsSmall().then(data => this.setState({ [`${productStateKey}`]: data }));
+        console.log(this.state);
+    }
 
-  const columns = useMemo(
-    () => [
-      {
-        accessorKey: 'id',
-        header: 'ID',
-        enableColumnOrdering: false,
-        enableEditing: false, //disable editing on this column
-        enableSorting: false,
-        size: 80,
-      },
-      {
-        accessorKey: 'firstName',
-        header: 'First Name',
-        size: 140,
-        muiTableBodyCellEditTextFieldProps: ({ cell }) => ({
-          ...getCommonEditTextFieldProps(cell),
-        }),
-      },
-      {
-        accessorKey: 'lastName',
-        header: 'Last Name',
-        size: 140,
-        muiTableBodyCellEditTextFieldProps: ({ cell }) => ({
-          ...getCommonEditTextFieldProps(cell),
-        }),
-      },
-      {
-        accessorKey: 'email',
-        header: 'Email',
-        muiTableBodyCellEditTextFieldProps: ({ cell }) => ({
-          ...getCommonEditTextFieldProps(cell),
-          type: 'email',
-        }),
-      },
-      {
-        accessorKey: 'age',
-        header: 'Age',
-        size: 80,
-        muiTableBodyCellEditTextFieldProps: ({ cell }) => ({
-          ...getCommonEditTextFieldProps(cell),
-          type: 'number',
-        }),
-      },
-      {
-        accessorKey: 'state',
-        header: 'State',
-        muiTableBodyCellEditTextFieldProps: {
-          select: true, //change to select for a dropdown
-          children: states.map((state) => (
-            <MenuItem key={state} value={state}>
-              {state}
-            </MenuItem>
-          )),
-        },
-      },
-    ],
-    [getCommonEditTextFieldProps],
-  );
+    isPositiveInteger(val) {
+        let str = String(val);
+        str = str.trim();
+        if (!str) {
+            return false;
+        }
+        str = str.replace(/^0+/, "") || "0";
+        let n = Math.floor(Number(str));
+        return n !== Infinity && String(n) === str && n >= 0;
+    }
 
-  return (
-    <>
-      <MaterialReactTable
-        displayColumnDefOptions={{
-          'mrt-row-actions': {
-            muiTableHeadCellProps: {
-              align: 'center',
-            },
-            size: 120,
-          },
-        }}
-        columns={columns}
-        data={tableData}
-        editingMode="modal" //default
-        enableColumnOrdering
-        enableEditing
-        onEditingRowSave={handleSaveRowEdits}
-        renderRowActions={({ row, table }) => (
-          <Box sx={{ display: 'flex', gap: '1rem' }}>
-            <Tooltip arrow placement="left" title="Edit">
-              <IconButton onClick={() => table.setEditingRow(row)}>
-                <Edit />
-              </IconButton>
-            </Tooltip>
-            <Tooltip arrow placement="right" title="Delete">
-              <IconButton color="error" onClick={() => handleDeleteRow(row)}>
-                <Delete />
-              </IconButton>
-            </Tooltip>
-          </Box>
-        )}
-        renderTopToolbarCustomActions={() => (
-          <Button
-            color="secondary"
-            onClick={() => setCreateModalOpen(true)}
-            variant="contained"
-          >
-            Create New Account
-          </Button>
-        )}
-      />
-      <CreateNewAccountModal
-        columns={columns}
-        open={createModalOpen}
-        onClose={() => setCreateModalOpen(false)}
-        onSubmit={handleCreateNewRow}
-      />
-    </>
-  );
-};
+    getStatusLabel(status) {
+        switch (status) {
+            case 'INSTOCK':
+                return 'In Stock';
 
-//example of creating a mui dialog modal for creating new rows
-export const CreateNewAccountModal = ({ open, columns, onClose, onSubmit }) => {
-  const [values, setValues] = useState(() =>
-    columns.reduce((acc, column) => {
-      acc[column.accessorKey ?? ''] = '';
-      return acc;
-    }, {}),
-  );
+            case 'LOWSTOCK':
+                return 'Low Stock';
 
-  const handleSubmit = () => {
-    //put your validation logic here
-    onSubmit(values);
-    onClose();
-  };
+            case 'OUTOFSTOCK':
+                return 'Out of Stock';
 
-  return (
-    <Dialog open={open}>
-      <DialogTitle textAlign="center">Create New Account</DialogTitle>
-      <DialogContent>
-        <form onSubmit={(e) => e.preventDefault()}>
-          <Stack
-            sx={{
-              width: '100%',
-              minWidth: { xs: '300px', sm: '360px', md: '400px' },
-              gap: '1.5rem',
-            }}
-          >
-            {columns.map((column) => (
-              <TextField
-                key={column.accessorKey}
-                label={column.header}
-                name={column.accessorKey}
-                onChange={(e) =>
-                  setValues({ ...values, [e.target.name]: e.target.value })
-                }
-              />
-            ))}
-          </Stack>
-        </form>
-      </DialogContent>
-      <DialogActions sx={{ p: '1.25rem' }}>
-        <Button onClick={onClose}>Cancel</Button>
-        <Button color="secondary" onClick={handleSubmit} variant="contained">
-          Create New Account
-        </Button>
-      </DialogActions>
-    </Dialog>
-  );
-};
+            default:
+                return 'NA';
+        }
+    }
 
-const validateRequired = (value) => !!value.length;
-const validateEmail = (email) =>
-  !!email.length &&
-  email
-    .toLowerCase()
-    .match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-    );
-const validateAge = (age) => age >= 18 && age <= 50;
+    onCellEditComplete(e) {
+        let { rowData, newValue, field, originalEvent: event } = e;
 
-export default Tablero;
+        switch (field) {
+            case 'quantity':
+            case 'price':
+                if (this.isPositiveInteger(newValue))
+                    rowData[field] = newValue;
+                else
+                    event.preventDefault();
+                break;
+
+            default:
+                if (newValue.trim().length > 0)
+                    rowData[field] = newValue;
+                else
+                    event.preventDefault();
+                break;
+        }
+    }
+
+    onRowEditComplete1(e) {
+        let products2 = [...this.state.products2];
+        let { newData, index } = e;
+
+        products2[index] = newData;
+
+        this.setState({ products2 });
+    }
+
+    onRowEditComplete2(e) {
+        let products3 = [...this.state.products3];
+        let { newData, index } = e;
+
+        products3[index] = newData;
+
+        this.setState({ products3 });
+    }
+
+    onRowEditChange(e) {
+        this.setState({ editingRows: e.data });
+    }
+
+    setActiveRowIndex(index) {
+        let editingRows = { ...this.state.editingRows, ...{ [`${this.state.products3[index].id}`]: true } };
+        this.setState({ editingRows });
+    }
+
+    cellEditor(options) {
+        if (options.field === 'price')
+            return this.priceEditor(options);
+        else
+            return this.textEditor(options);
+    }
+
+    textEditor(options) {
+        return <InputText type="text" value={options.value} onChange={(e) => options.editorCallback(e.target.value)} />;
+    }
+
+    statusEditor(options) {
+        return (
+            <Dropdown value={options.value} options={this.statuses} optionLabel="label" optionValue="value"
+                onChange={(e) => options.editorCallback(e.value)} placeholder="Select a Status"
+                itemTemplate={(option) => {
+                    return <span className={`product-badge status-${option.value.toLowerCase()}`}>{option.label}</span>
+                }} />
+        );
+    }
+
+    priceEditor(options) {
+        return <InputNumber value={options.value} onValueChange={(e) => options.editorCallback(e.value)} mode="currency" currency="USD" locale="en-US" />
+    }
+
+    statusBodyTemplate(rowData) {
+        return this.getStatusLabel(rowData.inventoryStatus);
+    }
+
+    priceBodyTemplate(rowData) {
+        return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(rowData.price);
+    }
+
+    render() {
+        return (
+            <div className="datatable-editing-demo">
+                <Toast ref={(el) => this.toast = el} />
+                <div className="card p-fluid">
+                    <h5>Cell Editing with Sorting and Filter</h5>
+                    <DataTable value={this.state.products4} editMode="cell" className="editable-cells-table" filterDisplay="row" responsiveLayout="scroll">
+                        {
+                            this.columns.map(({ field, header }) => {
+                                return <Column key={field} field={field} header={header} filter sortable style={{ width: '25%' }} body={field === 'price' && this.priceBodyTemplate}
+                                    editor={(options) => this.cellEditor(options)} onCellEditComplete={this.onCellEditComplete} />
+                            })
+                        }
+                    </DataTable>
+                </div>
+            </div>
+        );
+    }
+}
+                 
