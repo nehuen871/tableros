a3484f8b92ff1ef1d2f991f308bd62db27da7ecc
nenu <nehuennavarro@gmail.com>
2022-11-15 14:16:33 -0300

    test

--- react-backend/client/src/powerBi/powerBi.js
+++ react-backend/client/src/powerBi/powerBi.js
@@ -2,25 +2,19 @@ import React from 'react'
 import { PowerBIEmbed } from 'powerbi-client-react';
 import { models } from 'powerbi-client';
 import "../styles/powerBi.css"
-
+import { useLocation, useParams } from "react-router-dom";
 /*https://app.powerbi.com/reportEmbed?reportId=37f58113-3e68-464a-bba2-366538375822&autoAuth=true&ctid=2377472e-8004-4648-8456-bd9687afa150
 https://app.powerbi.com/view?r=eyJrIjoiZGQ0YWMwYTctZDZkNS00OTg3LWJlNmEtMDljY2VjNzBiMzUzIiwidCI6IjIzNzc0NzJlLTgwMDQtNDY0OC04NDU2LWJkOTY4N2FmYTE1MCIsImMiOjR9*/
-export default class PowerBiComponente extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    componentDidMount(){
-        console.log(this.props.match.params.id);
-    }
+class PowerBiComponente extends React.Component {
     render() {
         return(
             <div className="contnedorPowerBi col-md-12">
                 <PowerBIEmbed
                     embedConfig = {{
                         type: 'report',   // Supported types: report, dashboard, tile, visual and qna
-                        id: "a",//this.props.state,
-                        embedUrl: 'https://app.powerbi.com/view?r=',//+this.props.state,
-                        accessToken: this.props.state,
+                        id: this.props.params.id,
+                        embedUrl: 'https://app.powerbi.com/view?r='+this.props.params.token,
+                        accessToken: this.props.params.token,
                         tokenType: models.TokenType.Embed,
                         settings: {
                             panes: {
@@ -51,3 +45,7 @@ export default class PowerBiComponente extends React.Component {
         );
     }
 }
+
+export default () => (
+    <PowerBiComponente params={useParams()} location={useLocation()} />
+  );
